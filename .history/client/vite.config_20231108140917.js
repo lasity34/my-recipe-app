a43cbe81dbcb54ef5dfe9099f17a50b77d85dcbe
Bjorn Worrall <85297118+lasity34@users.noreply.github.com>
2023-11-21 14:33:19 +0200

    api setup

--- .history/client/vite.config_20231108140917.js
+++ .history/client/vite.config_20231108140917.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'https://boozy-benders.onrender.com'
+      '/api': 'http://localhost:3001'
     }
   }
 })
